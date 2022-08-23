9f2f08edb778c03d195c8dd814e59fd3b0b0bc58
raashid8 <aaminaraashid@gmail.com>
2022-08-23 12:49:04 +0530

    Updates

---
+++ precache-manifest.15f2f439ae76dd8e8940748adea2476b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "05df64bc30c24071ebe6",
+    "url": "/[raashid8].github.io/fonter-react/static/css/main.9899437b.chunk.css"
+  },
+  {
+    "revision": "05df64bc30c24071ebe6",
+    "url": "/[raashid8].github.io/fonter-react/static/js/main.05df64bc.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/[raashid8].github.io/fonter-react/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/[raashid8].github.io/fonter-react/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "c2129bb55aa322e11b40",
+    "url": "/[raashid8].github.io/fonter-react/static/js/runtime~main.c2129bb5.js"
+  },
+  {
+    "revision": "d72241a940f14af4ff1c49c2355fd2eb",
+    "url": "/[raashid8].github.io/fonter-react/index.html"
+  }
+];
